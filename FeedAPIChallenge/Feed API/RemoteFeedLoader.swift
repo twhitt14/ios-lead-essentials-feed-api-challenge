3e9373b95062718dc9d1de9b440c49d7767bc42e
Trevor Whittingham <twhittingham@ancestry.com>
2021-08-19 16:37:33 -0600

    Added JSON decoding logic to the RemoteFeedLoader load method, in order to make the test_load_deliversSuccessWithNoItemsOn200HTTPResponseWithEmptyJSONList method pass without breaking any existing tests.

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -23,9 +23,38 @@ public final class RemoteFeedLoader: FeedLoader {
 			switch result {
 			case .failure(_):
 				completion(.failure(Error.connectivity))
-			case .success(_):
-				completion(.failure(Error.invalidData))
+			case .success((let data, let response)):
+				guard response.statusCode == 200,
+				      let responseObject = try? JSONDecoder().decode(FeedImageResponseObject.self, from: data)
+				else {
+					completion(.failure(Error.invalidData))
+					return
+				}
+
+				completion(Result.success(responseObject.itemsAsFeedImageArray()))
 			}
+		}
+	}
+
+	private struct FeedImageResponseObject: Decodable {
+		let items: [FeedImageObject]
+
+		func itemsAsFeedImageArray() -> [FeedImage] {
+			items.map { $0.asFeedImage() }
+		}
+	}
+
+	private struct FeedImageObject: Decodable {
+		let image_id: UUID
+		let image_desc: String?
+		let image_loc: String?
+		let image_url: URL
+
+		func asFeedImage() -> FeedImage {
+			FeedImage(id: image_id,
+			          description: image_desc,
+			          location: image_loc,
+			          url: image_url)
 		}
 	}
 }
